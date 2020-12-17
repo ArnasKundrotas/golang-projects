61b36fa1ae4668938774c19b26a0165e1c9ec133
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-17 14:25:03 +0200

    edit card main.go

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	//var card string = "Ace of Spades"
-	card := newCard() // := new variable assignment
-	//card = "Five of Diamonds" = for existing variables no :
-	fmt.Println(card) // import "fmt" on save
+	cards := []string{newCard(), newCard()}
 }
 
 func newCard() string {
