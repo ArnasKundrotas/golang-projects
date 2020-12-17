ef48a4cc53da972e52706aecc78895b9bcb7c6fb
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-17 16:05:05 +0200

    update card project

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,21 @@ import "fmt"
 // Create a new type of 'deck
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuit := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValue := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuit {
+		for _, value := range cardValue {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
