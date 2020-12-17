38d1eb2643543c836c638bad06b408e70599c215
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-17 14:13:48 +0200

    add cards project star

--- cards/main.go
+++ cards/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades" // := new variable assignment
+	card := newCard() // := new variable assignment
 	//card = "Five of Diamonds" = for existing variables no :
 	fmt.Println(card) // import "fmt" on save
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
